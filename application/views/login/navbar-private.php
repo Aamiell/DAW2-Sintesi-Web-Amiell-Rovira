f974a9b663bf081a45d4147cbeba7e8da2332246
DaniRovira <59705842+DaniRovira@users.noreply.github.com>
2021-04-22 16:40:12 +0200

    second commit

--- application/views/login/navbar-private.php
+++ application/views/login/navbar-private.php
@@ -17,7 +17,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-user-friends"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3"> AMIELL&ROVIRA
+                <div class="sidebar-brand-text mx-3"> AMIELL&ROVIR
                     <!--<sup>3</sup>-->
                 </div>
             </a>
